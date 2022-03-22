7d11faffb437c6900ae2f4a5ad40ac587a252832
Fredbear07 <maggioludanilo@gmail.com>
2022-03-22 20:46:32 +0100

    -

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from difflib import context_diff
 from multiprocessing import context
+from unicodedata import name
 import discord
 from discord.ext import commands
 import os
@@ -7,6 +8,8 @@ from dotenv import load_dotenv
 import logging
 import random
 import asyncio
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
 
 load_dotenv()
 
@@ -15,6 +18,7 @@ Prefix = os.getenv('PREFIX')
 
 client = discord.Client()
 bot = commands.Bot(command_prefix=Prefix)
+slash = SlashCommand(bot, sync_commands=True)
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -26,19 +30,42 @@ logger.addHandler(handler)
 async def on_ready():
     print('Bot ready')
 
-@bot.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    await bot.process_commands(message)
+# @bot.event
+# async def on_message(message):
+#     if message.author =   = client.user:
+#         return
+#     await bot.process_commands(message)
 
-@bot.command(pass_context=True, aliases=['orso','Orso'])
-async def cmd1(ctx):
+# @bot.command(pass_context=True, aliases=['orso','Orso'])
+# async def cmd1(ctx):
+#     orso1 = random.randint(1,8)
+#     orso2 = str(orso1)
+#     embed1 = discord.Embed(title="Ecco un orso a caso (Orso numero " + orso2 + ")")
+#     embed1.set_image(url="https://raw.githubusercontent.com/Fredbear07/Bears/main/Bears/Bear" + orso2 + ".png")
+#     await ctx.send(embed=embed1)
+
+@slash.slash(
+    name="Orso",
+    description="8 Foto di orsi casuali",
+    guild_ids=[875826540523429929]    
+)    
+async def _orso(ctx: SlashContext):
     orso1 = random.randint(1,8)
     orso2 = str(orso1)
-    embed1 = discord.Embed(title="Ecco un orso a caso")
+    embed1 = discord.Embed(title="Ecco un orso a caso (Orso numero " + orso2 + ")")
     embed1.set_image(url="https://raw.githubusercontent.com/Fredbear07/Bears/main/Bears/Bear" + orso2 + ".png")
     await ctx.send(embed=embed1)
 
+# @slash.slash(
+#     name="Orsi",
+#     description="elenco orsi",
+#     guild_ids=[875826540523429929]    
+# )    
+# async def _orsi(ctx: SlashContext):
+#     orso1 = random.randint(1,8)
+#     orso2 = str(orso1)
+#     embed1 = discord.Embed(title="Ecco un orso a caso (Orso numero " + orso2 + ")")
+#     embed1.set_image(url="https://raw.githubusercontent.com/Fredbear07/Bears/main/Bears/Bear" + orso2 + ".png")
+#     await ctx.send(embed=embed1)
 
 bot.run(Ds_Token)
